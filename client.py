d916f955fac859f7850e06eb68b3a65135f60965
Ryan-Chowdhary <94214471+Ryan-Chowdhary@users.noreply.github.com>
2022-07-17 13:51:13 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,44 +1,85 @@
-from logging import error
 import socket
-import time
+import json
 import os
+import hashlib
 
-host= 'localhost'
-port= 5000
+data = json.load(open('../host.json'))
+settings= data['service_setting'][0]
+host= settings['host']
+port= settings['port']
+#host= 'localhost'
+#port= 5000
+
+def close():
+    try:
+        s.close()
+        print('~server disconnected!')
+        exit()
+    except Exception:
+        exit()
+
+def create_dir(dir):
+    try:
+        os.mkdir(dir)
+    except FileExistsError:
+        os.chdir(dir)
 
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.connect((host, port))
 except ConnectionRefusedError as err:
     print(err, 'it seems that the server is down or unreachable', sep='\n')
+    s.close()
     exit()
 print('Connected to server!')
-file_tree= s.recv(2048*10)
-file_tree= file_tree.decode('utf-8', 'ignore')
+file_tree= s.recv(2048*10).decode('utf-8', 'ignore')
 print(file_tree)
 filename= input('filename: ')
 path, file_name= os.path.split(filename)
 s.send(filename.encode())
-    # FILE EXISTS == F.E
-    # FILE DOES NOT EXIST == F.NE
+# FILE EXISTS == F.E
+# FILE DOES NOT EXIST == F.NE
+# REQ. FILE IS A DIRECTORY == F.ID
 try:
     file_status= s.recv(1024)
     if file_status.decode() == 'F.E':
         pass
     elif file_status.decode() == 'F.NE':
-        s.close()
         print('file does not exist on server')
-        print('~server disconnected!')
-        exit()
+        close()
+    elif file_status.decode() == 'F.ID':
+        print('requested file is a directory!')
+        close()
 except Exception as err:
     print(err, 'an exception has occured at line 19', sep='\n')
-content= s.recv(1024)
+
+file_id, file_ext = os.path.splitext(file_name)
+file_contents_list = []
+
+create_dir(file_id)
+
 file= open(file_name, 'wb')
-while content:
+length = int(s.recv(1024).decode())
+content= s.recv(1024)
+for i in range(length):
+    file_contents_list.append(content)
     print(content.__sizeof__(), 'bytes recived')
     file.write(content)
     content= s.recv(1024)
-file.close()
 print(content.__sizeof__(), 'bytes recived')
+file.close()
 print('file recieved')
-s.close()
+# Hashing recived file
+print('verifying integrity of recived file')
+sha256_hash = hashlib.sha256()
+for i in file_contents_list:
+    sha256_hash.update(i)
+new_file_hash = str(sha256_hash.hexdigest())
+original_hash = s.recv(1024).decode()
+s.send(new_file_hash.encode())
+if str(new_file_hash) == str(original_hash):
+    print('hashes match! File validity confrmed')
+else:
+    print('Error, hashes do not match, it seems that you have recived an bad file.')
+
+close()
--- server.py
+++ server.py
@@ -1,32 +1,36 @@
-
 import socket
-import time
+import json
 import os
+import hashlib
 
-host= 'localhost'
-port= 5000
+data = json.load(open('host.json'))
+admin= data['login_detail'][0]
+settings= data['service_setting'][0]
+host= settings['host']
+port= settings['port']
+#host= 'localhost'
+#port= 5000
 
 def close():
     try:
         s.shutdown(socket.SHUT_RDWR)
+        print('~closing connection!')
         s.close()
         exit()
     except Exception as err:
-        s.close()
-        print('wait until the service is free\'ed up')
+        print(err)        
         exit()
 
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-running= True
-while running:
-    try:
-        s.bind((host, port))
-        print('server started on', (host, port))
-        running= False
-    except OSError as err:
-        print(err)
-        s.close()
-        exit()
+
+try:
+    s.bind((host, port))
+    print('server started on', (host, port))
+    running= False
+except OSError as err:
+    print(err)
+    print('wait until the service is free\'ed up')
+    exit()
     
 s.listen(5)
 try:
@@ -42,26 +46,43 @@ try:
     file_content = []
     # FILE EXISTS == F.E
     # FILE DOES NOT EXIST == F.NE
+    # REQ. FILE IS A DIRECTORY == F.ID
     try:
         file = open(fname, 'rb')
         c.send(b'F.E')
     except FileNotFoundError as err:
         print(err)
         c.send(b'F.NE')
-        print('~closing connection!')
+        close()
+    except IsADirectoryError as err:
+        print(err)
+        c.send(b'F.ID')
         close()
     con = ''
     while con != b'':
         con = file.readline(1024)
         file_content.append(con)
     print(f'sending file {file_name} to client {addr}')
+    c.send(str(file_content.__len__()).encode())
     for i in file_content:
         #time.sleep(0.01)
         c.send(i)
-        if i.__sizeof__() >= 1024:
-            print(i.__sizeof__(), 'bytes sent')
+        print(i.__sizeof__(), 'bytes sent')
     file.close()
     print(f'file {file_name} sent to client {addr}')
+    print('verifying file sent to client', addr)
+    # Hashing sent file
+    sha256_hash = hashlib.sha256()
+    for i in file_content:
+        sha256_hash.update(i)
+    #print(sha256_hash.hexdigest())
+    hash = str(sha256_hash.hexdigest())
+    c.send(hash.encode())
+    new_file_hash = c.recv(1024).decode()
+    if str(hash) == str(new_file_hash):
+        print('hashes match! File validity confrmed')
+    else:
+        print('Error, hashes do not match, client has an invalid file.')
     close()
 except KeyboardInterrupt as intr:
     print(intr)
